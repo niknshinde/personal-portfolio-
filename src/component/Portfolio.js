cef74e06f2649aab0f9dd07dcd18fc289ce4a7ad
niknshinde <ns8830603789@gmail.com>
2024-01-14 17:54:45 +0530

    latest update 14 jan 2024

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -25,6 +25,18 @@ const Portfolio = () => {
         {/* second box */}
 
         <div className="portfolio_box">
+            <img className='portimg' src="/images/pimg4.png" alt="pimg4" />
+            <div className="title">
+                   <h2>Cloud Notes</h2>
+                <div className='portfolio_button'>
+                <a className='aformpbn1' href="https://github.com/niknshinde/Cloud-Notes" target="_blank"><button className='pbtn pbtn1' >GitHub</button></a>
+                    <a href="https://cloud-note-zs6x.onrender.com/"><button className='pbtn pbtn2'>Live Demo</button></a>
+                </div>
+            </div>
+         </div>
+
+
+        <div className="portfolio_box">
             <img className='portimg' src="/images/pimg3.png" alt="pimg1" />
             <div className="title">
                    <h2>News website</h2>
@@ -35,17 +47,19 @@ const Portfolio = () => {
             </div>
          </div>
 
-
-         {/* <div className="portfolio_box">
-            <img className='portimg' src="/images/pimg3.png" alt="pimg1" />
+         <div className="portfolio_box">
+            <img className='portimg' src="/images/pimg5.png" alt="pimg1" />
             <div className="title">
-                   <h2>News website</h2>
+                   <h2>Talentsphere</h2>
                 <div className='portfolio_button'>
-                <a className='aformpbn1' href="https://github.com/niknshinde/info-wave-website" target="_blank"><button className='pbtn pbtn1' >GitHub</button></a>
-                    <a href="https://cat-bucket.netlify.app/"><button className='pbtn pbtn2'>Live Demo</button></a>
+                <a className='aformpbn1' href="https://github.com/niknshinde/job_find_portal" target="_blank"><button className='pbtn pbtn1' >GitHub</button></a>
+                    <a href="https://talentsphere-xnfy.onrender.com/"><button className='pbtn pbtn2'>Live Demo</button></a>
                 </div>
             </div>
-         </div> */}
+         </div>
+
+
+        
 
     </div>
     </div>
